db7f99dc0e0a4d98f67b4e82b3cd6922654c779b
Jeff Anderson <jeff.anderson867@gmail.com>
2017-06-14 11:20:58 -0400

    Initial Commit

---
+++ Egg Timer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Egg Timer
+//
+//  Created by Big J on 6/14/17.
+//  Copyright © 2017 AndersonCoding. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Egg Timer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by Big J on 6/14/17.
+//  Copyright © 2017 AndersonCoding. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
